643ba098c6c451d78efb2d71a068d889156eafcb
Adriano Goncalves <amg1976@gmail.com>
2016-02-13 16:22:12 +0000

    Added a ViewModel for MainMoviesList

---
+++ ViperPrototype/Modules/MainMoviesList/View/MainMoviesListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainMoviesListViewItem.swift
+//  ViperPrototype
+//
+//  Created by Adriano Goncalves on 13/02/2016.
+//  Copyright © 2016 Adriano Goncalves. All rights reserved.
+//
+
+import Foundation
+
+struct MainMoviesListViewItem {
+   var totalMoviesString: String
+}
