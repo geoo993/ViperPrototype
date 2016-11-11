5f580c7214f57e21bc3f2e9c80f6df73a31294fc
Daniel Asher <daniel.asher.2303@gmail.com>
2016-11-11 11:50:04 +0000

    fix FIXME: 01 - SWIFT-3 mirgration break

--- ViperPrototype/Modules/MainMoviesList/View/MainMoviesListView.swift
+++ ViperPrototype/Modules/MainMoviesList/View/MainMoviesListView.swift
@@ -40,29 +40,23 @@ class MainMoviesListView: UIViewController {
 
       collectionView.setCollectionViewLayout(MainMoviesListCollectionFLowLayout(), animated: false)
       
-//    let a = collectionView.rx.items(cellIdentifier: MainMoviesListCell.identifier, cellType: MainMoviesListCell.self)
-    
-    //{ (_:Int, model: MainMoviesListViewModel, cell: MainMoviesListCell) in
-//        model.backdropImage = ImagesService.sharedInstance.downloadImage(model.movieBackdropUrl)
-//        cell.updateUI(model)
-//    }
-    
-    // FIXME: ❗️SWIFT-3 migration break
     if let presenter = presenter {
          let moviesObserver = presenter.gotAllMovies().shareReplay(1)
-//         moviesObserver
-//            .map({ $0.totalMoviesString })
-//            .observeOn(MainScheduler.instance)
-//            .bindTo(totalMoviesLabel.rx.text)
-//            .addDisposableTo(disposeBag)
-//         moviesObserver
-//            .map({ $0.movies })
-//            .observeOn(MainScheduler.instance)
-//            .bindTo(collectionView.rx.items(cellIdentifier: MainMoviesListCell.identifier, cellType: MainMoviesListCell.self) { (_:Int, model: MainMoviesListViewModel, cell: MainMoviesListCell) in
-//               model.backdropImage = ImagesService.sharedInstance.downloadImage(model.movieBackdropUrl)
-//               cell.updateUI(model)
-//            })
-//            .addDisposableTo(disposeBag)
+         moviesObserver
+            .map({ $0.totalMoviesString })
+            .observeOn(MainScheduler.instance)
+            .bindTo(totalMoviesLabel.rx.text)
+            .addDisposableTo(disposeBag)
+         moviesObserver
+            .map({ $0.movies })
+            .observeOn(MainScheduler.instance)
+            .bindTo(collectionView.rx.items(cellIdentifier: MainMoviesListCell.identifier, cellType: MainMoviesListCell.self))
+            { (_, model, cell) in
+                var newModel = model
+               newModel.backdropImage = ImagesService.sharedInstance.downloadImage(model.movieBackdropUrl)
+               cell.updateUI(newModel)
+            }
+            .addDisposableTo(disposeBag)
       }
    }
 
